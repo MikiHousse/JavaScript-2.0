e18e63cbb4b7082c31fa82de78bf615de989ee5c
Mikhail Babaychev <109237708+MikiHousse@users.noreply.github.com>
2022-11-09 13:42:27 +0300

    Решил 13 задач из курса

---
+++ UNIT 2 ocnovi voda dannix/js/script.js
@@ -0,0 +1,90 @@
+// остановился на 14 задании
+
+let r = 6;
+let t = 'Hello';
+
+// console.log(a);
+
+// let inputIn = document.querySelector('.input-in'); // input
+// let inputInTwo = document.querySelector('.input-in-two');
+let button = document.querySelector('button'); // button
+let div = document.querySelector('.out');
+let inputIn = document.getElementsByClassName('.input-in').value = 'Hello';
+
+// button.onclick = function () {
+//     // кнопка будет нажата
+//     console.log('Работает!!!!!!');
+//     // console.log(inputIn.value); // value - то, что введено в input
+//     let b = +inputIn.value; // + перевести в число!!!!!!!
+//     console.log(b + 10); // '66'+10 = 6610
+//     div.innerHTML = b;
+//     inputIn.value = '';
+// }
+
+button.addEventListener = ('click', () => {
+    console.log('work');
+    // let b = +inputIn.value;
+    // let a = +inputInTwo.value;
+    // console.log(a + b)
+    // div.innerHTML = a + b;
+    inputIn.value = 'hello';
+    // inputIn.setAttribute('value', inputIn.value);
+    console.log(inputIn.value)
+    // inputInTwo.value = '';
+
+})
+
+
+
+
+// 1 задача
+
+let a = 7;
+let b = 9;
+
+console.log(a * b);
+
+// 2 задача
+
+let c = 7;
+let d = 9;
+
+console.log(c / d);
+
+// 3 задача 
+
+let e = 3;
+let f = 5;
+
+console.log(e + f);
+
+// 4 задача
+
+let e1 = '3'; // javascrip преобразовал число в строку 
+let f1 = 5;
+
+console.log(e1 + f1);
+
+// 5 задача
+
+let e2 = 3;
+let f2 = 0;
+
+console.log(e2 / f2); // бесконечность 
+
+// 6 задача
+
+let e3 = 3;
+let f3 = 'Hello'; 
+
+console.log(e3 + f3); // когда в выражение есть строка то другая переменная становиться тоже строкой
+
+// 7 задача
+
+let e4 = 3;
+let f4 = 'Hello';
+
+console.log(e4 * f4); // нельзя умножать строку на число
+
+// 8 задача
+
